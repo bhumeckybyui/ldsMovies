86c945edbf19b833792164a5828f82707920bf93
bhumecky <daddyhumecky@gmail.com>
2018-04-02 14:15:07 -0600

    first push

---
+++ LdsMovies/Models/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LdsMovies.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LdsMovies.Models.Movie> Movie { get; set; }
+    }
+}
